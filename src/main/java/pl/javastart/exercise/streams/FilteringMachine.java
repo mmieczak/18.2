a16a240d6b1ac24b70a2a54d05f4c4b0b4ee24e0
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-06-09 12:28:11 +0200

    Ready for code review

--- src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -1,23 +1,36 @@
 package pl.javastart.exercise.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachine {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+        List<Integer> collectionToFilter = numberList.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toList());
+        return collectionToFilter;
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+        List<Integer> collectionToFilter2 = numberList.stream()
+                .filter(number -> number > 20)
+                .collect(Collectors.toList());
+        return collectionToFilter2;
     }
 
     public List<Book> convertToBooks(List<String> titles) {
-        return null;
+        List<Book> bookList = titles.stream()
+                .map(Book::new)
+                .collect(Collectors.toList());
+        return bookList;
     }
 
     //Metoda powinna przekształcić tytuły na książki i zwrócić tylko te które rozpoczynają się od słowa "Gra"
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
-        return null;
+        List<Book> booksStaretingWithGra = convertToBooks(titles).stream()
+                .filter(name -> name.getTitle().startsWith("Gra"))
+                .collect(Collectors.toList());
+        return booksStaretingWithGra;
     }
 }
